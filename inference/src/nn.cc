e8d2f4bf59ec341b75b30d1e5e20c7746b40b3e0
GilesStrong <giles.strong@outlook.com>
2020-01-08 12:31:09 +0000

    checkpoint

--- inference/src/nn.cc
+++ inference/src/nn.cc
@@ -5,7 +5,9 @@ NN::NN(std::string root_name, unsigned int n_threads, bool verbose=false) {
 
     _verbose = verbose;
     _n_threads = n_threads;
+    if (_verbose) std::cout << "Loading model";
     assert(NN::load_model(root_name));
+    if (_verbose) std::cout << "Model loaded";
 }
 
 NN::~NN() {
@@ -19,17 +21,17 @@ bool NN::load_model(std::string root_name) {
         throw std::invalid_argument("File: " + root_name + ".pb not found");
         return false;
     }
-    if (_verbose) std::cout << "Loading model...";
+    if (_verbose) std::cout << "Loading protobuf...";
     _model = tensorflow::loadGraphDef(root_name + ".pb");
-    if (_verbose) std::cout << "\tModel loaded\n";
+    if (_verbose) std::cout << "\tprotobuf loaded\n";
 
      _input_name  = _model->node(0).name();
      _output_name = _model->node(_model->node_size()-1).name();
 
     if (_verbose) {
-        std::cout << "Model:\n______________________________\n______________________________\n";
+        std::cout << "Model:\n______________________________\n";
         for (int i = 0; i < _model->node_size(); i++) std::cout << "Tensor " << i << " name " <<  _model->node(i).name() << "\n";
-        std::cout << "Model:\n______________________________\n______________________________\n";
+        std::cout << "______________________________\n";
         std::cout << "Using " << _input_name << " as input and " << _output_name << " as output\n";
     }
     return true;
